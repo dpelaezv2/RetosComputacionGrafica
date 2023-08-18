e76ef4d1ad1f402def1999f9884d773a4042f8c5
dpelaezv2 <dpelaezv2@eafit.edu.co>
2023-08-18 16:39:24 -0500

    Reto1 y Reto2

---
+++ Reto1/Draw.java
@@ -0,0 +1,106 @@
+package Retos.Reto1;
+
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.awt.Color;
+import java.awt.Graphics;
+import javax.swing.JPanel;
+import javax.swing.JFrame;
+
+public class Draw extends JPanel {
+    public static final int WIDTH = 1000;
+    public static final int HEIGHT = 700;
+    String fileName = "Retos/Reto1/gancho.txt";
+
+    public static void main(String [] args) {
+        // Crear un nuevo Frame
+        JFrame frame = new JFrame("Puntos");
+        // Al cerrar el frame, termina la ejecución de este programa
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // Agregar un JPanel que se llama Points (esta clase)
+        frame.add(new Draw());
+        // Asignarle tamaño
+        frame.setSize(WIDTH, HEIGHT);
+        // Poner el frame en el centro de la pantalla
+        frame.setLocationRelativeTo(null);
+        // Mostrar el frame
+        frame.setVisible(true);
+    }    
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        g.setColor(Color.blue);
+        
+        drawAxis(g);
+
+        g.setColor(Color.black);
+        Edge[] edges = readFile(fileName);
+        for (int i = 0; i < edges.length; i++) {
+            myDrawLine(g, edges[i].p1.x, edges[i].p1.y, edges[i].p2.x, edges[i].p2.y);
+        }
+    }
+
+    public void drawAxis(Graphics g) {
+        g.setColor(Color.red);
+        myDrawLine(g, -300, 0, 300, 0);
+        g.setColor(Color.green);
+        myDrawLine(g, 0, -300, 0, 300);
+    }
+
+    public void myDrawLine(Graphics g, int x1, int y1, int x2, int y2) {
+        int xj1 = x1 + WIDTH/2;
+        int yj1 = HEIGHT/2 - y1;
+        int xj2 = x2 + WIDTH/2;
+        int yj2 = HEIGHT/2 - y2;
+        g.drawLine(xj1, yj1, xj2, yj2);
+    }
+
+    public static Edge[] readFile(String fileName) {
+        try {
+            Scanner scanner = new Scanner(new File(fileName));
+            int numPoints = scanner.nextInt();
+            Point[] points = new Point[numPoints];
+            for(int i = 0; i < numPoints; i++ ) {
+                int x = scanner.nextInt();
+                int y = scanner.nextInt();
+                points[i] = new Point(x, y); 
+                System.out.println("Point " + i + ": (" + x + ", " + y + ")");
+            }
+            int numEdges = scanner.nextInt();
+            Edge[] edges = new Edge[numEdges];
+            for(int i = 0; i < numEdges; i++ ) {
+                int indice1 = scanner.nextInt();
+                int indice2 = scanner.nextInt();
+                edges[i] = new Edge(points[indice1], points[indice2]);
+                System.out.println("Desde: " + indice1 + " hasta: " + indice2);
+            }
+            scanner.close();
+            return edges;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
+
+class Point {
+    int x, y;
+
+    //Constructor
+    public Point (int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+class Edge {
+    Point p1, p2;
+
+    public Edge (Point p1, Point p2) {
+        this.p1 = p1;
+        this.p2 = p2;
+    }
+}
---
+++ Reto2/Llanta.java
@@ -0,0 +1,122 @@
+package Retos.Reto2;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+import javax.swing.WindowConstants;
+import javax.swing.JFrame;
+import java.awt.Dimension;
+
+public class Llanta {
+    /*public static final int WIDTH = 1000;
+    public static final int HEIGHT = 700;*/
+
+    String fileName = "Retos/Reto1/gancho.txt";
+
+    public static void main(String [] args) {
+        SwingUtilities.invokeLater(Llanta::run);
+    }   
+    
+    private static void run() {
+        JFrame f = new JFrame();
+        f.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        f.setTitle("Llanta");
+
+        f.getContentPane().add(new BresenhamPanel());
+        f.pack();
+
+        f.setLocationRelativeTo(null);
+        f.setVisible(true);
+    }
+}
+
+class BresenhamPanel extends JPanel {
+    private final int pixelSize = 1;
+
+    BresenhamPanel() {
+        setPreferredSize(new Dimension(600, 500));
+        setBackground(Color.WHITE);
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        
+        drawAxis(g);
+
+        int radio = 160;
+        g.setColor(Color.black);
+        for (int i = 0; i < 360; i += 10) {
+            myDrawLine(g, 0, 0, 
+                (int)(radio * Math.cos(Math.toRadians(i))), 
+                (int)(radio * Math.sin(Math.toRadians(i)))
+            );
+        }
+    }
+
+    public void drawAxis(Graphics g) {
+        g.setColor(Color.red);
+        myDrawLine(g, -200, 0, 200, 0);
+        g.setColor(Color.green);
+        myDrawLine(g, 0, -200, 0, 200);
+    }
+
+    private void myDrawLine(Graphics g, int x1, int y1, int x2, int y2) {
+        // delta of exact value and rounded value of the dependent variable
+        int d = 0;
+
+        int dx = Math.abs(x2 - x1);
+        int dy = Math.abs(y2 - y1);
+
+        int dx2 = 2 * dx; // slope scaling factors to
+        int dy2 = 2 * dy; // avoid floating point
+
+        int ix = x1 < x2 ? 1 : -1; // increment direction
+        int iy = y1 < y2 ? 1 : -1;
+
+        int x = x1;
+        int y = y1;
+
+        if (dx >= dy) {
+            while (true) {
+                plot(g, x, y);
+                if (x == x2)
+                    break;
+                x += ix;
+                d += dy2;
+                if (d > dx) {
+                    y += iy;
+                    d -= dx2;
+                }
+            }
+        } else {
+            while (true) {
+                plot(g, x, y);
+                if (y == y2)
+                    break;
+                y += iy;
+                d += dx2;
+                if (d > dy) {
+                    x += ix;
+                    d -= dy2;
+                }
+            }
+        }
+    }
+
+    private void plot(Graphics g, double x, double y) {
+        int w = (getWidth() - 1) / pixelSize;
+        int h = (getHeight() - 1) / pixelSize;
+        int maxX = (w - 1) / 2;
+        int maxY = (h - 1) / 2;
+
+        int borderX = getWidth() - ((2 * maxX + 1) * pixelSize + 1);
+        int borderY = getHeight() - ((2 * maxY + 1) * pixelSize + 1);
+        int left = (int)(x + maxX) * pixelSize + borderX / 2;
+        int top = (int)(y + maxY) * pixelSize + borderY / 2;
+
+        g.setColor(Color.black);
+        g.drawOval(left, top, pixelSize, pixelSize);
+    }
+}
